36d6f8a3ea9676edcae9feb5fcd398eadcccbc32
Ryll Barroso <124801279+Ryllary@users.noreply.github.com>
2024-05-17 20:59:25 -0300

    Add files via upload

---
+++ exe02/exe02/RegistroDeCompras.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe02
+{
+    class RegistroDeCompras
+    {
+        // Definição dos atributos 
+        private DateTime data;
+        private string produto;
+        private decimal valor;
+
+        // Lista para armazenar as compras
+        // list é uma lista  que podemos manipulá-los conforme necessário, apenas o que esta dentro
+        private List<RegistroDeCompras> compras;
+
+        // Construtor da classe
+        public RegistroDeCompras(DateTime data, string produto, decimal valor)
+        {
+            // this  referência para o objeto atual, tbm para identificar os objetos
+            this.data = data;
+            this.produto = produto;
+            this.valor = valor;
+            this.compras = new List<RegistroDeCompras>();
+        }
+
+        // Método para adicionar uma compra à lista
+        public void AdicionarCompra(DateTime data, string produto, decimal valor)
+        { // add é para adicionar 
+            compras.Add(new RegistroDeCompras(data, produto, valor));
+        }
+
+        // Método para listar todas as compras registradas
+        public void ListarCompras()
+        {
+            // foreach, você pode acessar e manipular cada elemento de uma lista, não deixando o código poluido
+            foreach (var compra in compras)
+            {
+                Console.WriteLine($"Data: {compra.data}, Produto: {compra.produto}, Valor: {compra.valor}");
+            }
+        }
+    }
+}
