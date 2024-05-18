7394fb682781dfdba1f589d1e0488ced859d1cea
Ryll Barroso <124801279+Ryllary@users.noreply.github.com>
2024-05-17 21:11:25 -0300

    Add files via upload

---
+++ exe08/exe08/Produto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe08
+{
+    public class Produto
+    {
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    nome = value;
+                }
+                else
+                {
+                    throw new ArgumentException("O nome do produto não pode ser vazio.");
+                }
+            }
+        }
+        public Fabricante Fabricante { get; set; }
+        private decimal preco;
+        public decimal Preco
+        {
+            get { return preco; }
+            set
+            {
+                if (value >= 0)
+                {
+                    preco = value;
+                }
+                else
+                {
+                    throw new ArgumentException("O preço do produto deve ser positivo.");
+                }
+            }
+        }
+    }
+}
